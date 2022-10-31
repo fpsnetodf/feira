b24df54a87e4c6b885ad2f0b01b9926de54738c7
fpsnetodf <fps_netodf@hotmail.com>
2022-10-31 19:05:13 -0300

    django-on-heroku

--- conf_Feira/settings.py
+++ conf_Feira/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'grafico'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
+
+# django-on-heroku
+django_on_heroku.settings(locals())
