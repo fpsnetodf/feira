5b3701ce924005e6611bf0d68f58db1ccd1244bf
Neto <81028868+fpsnetodf@users.noreply.github.com>
2022-11-03 14:34:18 -0300

    voltando ao true

--- conf_Feira/settings.py
+++ conf_Feira/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m6bs9xb4j8@96$k#4uwo^80v6bmh%&!8__rz2rmvj8(@a0b*u%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
